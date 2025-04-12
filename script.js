2bd9415cea5e2cc98d7a001c8931b20fe46f805e
Vaibhav Suryavanshi <23203009@mail.jiit.ac.in>
2025-04-12 16:28:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Just a simple fade animation when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".hero-content").classList.add("fade-in");
+  });
+  
